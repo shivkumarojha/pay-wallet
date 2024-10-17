ec10e53aff37cb6f170432b525761bc8dbec8bc5
shivkumarojha <ojhashivkumar88@gmail.com>
2024-10-17 17:48:00 +0530

    init and added tailwind in both project

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image"
+import { Button } from "@repo/ui/button"
+import styles from "./page.module.css"
+
+export default function Home() {
+  return <h2 className="bg-slate-500 text-yellow-400">hELLO WORLD</h2>
+}
